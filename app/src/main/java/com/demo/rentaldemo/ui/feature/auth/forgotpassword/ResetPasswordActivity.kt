d9b42315204850887154a01640a0ad70cbaad5ad
Evolve <evolve@Evolves-MacBook-Pro.local>
2023-10-16 08:54:00 +0545

    reset password design and packages added

--- app/src/main/java/com/demo/rentaldemo/ui/feature/auth/forgotpassword/ResetPasswordActivity.kt
+++ app/src/main/java/com/demo/rentaldemo/ui/feature/auth/forgotpassword/ResetPasswordActivity.kt
@@ -5,30 +5,27 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.demo.rentaldemo.R
-import com.demo.rentaldemo.databinding.ActivityVerifyEmailBinding
+import com.demo.rentaldemo.databinding.ActivityResetPasswordBinding
 import com.demo.rentaldemo.ui.base.BaseActivity
+import com.demo.rentaldemo.ui.feature.auth.login.LoginActivity
 
-class VerifyEmailActivity : BaseActivity<ActivityVerifyEmailBinding>() {
+class ResetPasswordActivity : BaseActivity<ActivityResetPasswordBinding>() {
 
     private val viewModel: ForgotPasswordViewModel by viewModels()
 
     companion object {
         fun start(activity: Activity) {
-            val intent = Intent(activity, VerifyEmailActivity::class.java)
+            val intent = Intent(activity, ResetPasswordActivity::class.java)
             activity.startActivity(intent)
         }
     }
 
-    override fun layout() = R.layout.activity_verify_email
+    override fun layout() = R.layout.activity_reset_password
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.tvBackToLogin.setOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
-        }
-
-        binding.btnVerify.setOnClickListener {
-            ResetPasswordActivity.start(this)
+        binding.btnConfirm.setOnClickListener {
+            LoginActivity.start(this)
         }
     }
 
